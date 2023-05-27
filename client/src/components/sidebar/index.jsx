e1e9a4f33a251b65c3a072223d5077cb600c0f8c
DinhTheCuong <cuongdinh733@gmail.com>
2023-05-27 14:05:37 +0700

    commit 4

--- client/src/components/sidebar/index.jsx
+++ client/src/components/sidebar/index.jsx
@@ -1,81 +1,111 @@
 import React from 'react';
 import './style.css';
-import {Link} from 'react-router-dom';
-import {RiArrowDropRightLine} from 'react-icons/ri';
-import {CiMobile3} from 'react-icons/ci';
-import {MdClose} from 'react-icons/md';
-import {CiLaptop} from 'react-icons/ci';
-import {IoGameControllerOutline} from 'react-icons/io5';
-import {CiPalette} from 'react-icons/ci';
+import { Link } from 'react-router-dom';
+import { RiArrowDropRightLine } from 'react-icons/ri';
+import { RiAccountCircleFill } from 'react-icons/ri';
+import { MdClose } from 'react-icons/md';
+import { HiHome } from 'react-icons/hi';
+import { AiFillShop } from 'react-icons/ai';
+import { MdContactEmergency } from 'react-icons/md';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
 
 const Sidebar = () => {
   const handleCloseSidebar = () => {
-    const x = (document.getElementById(
-      'sidebar',
-    ).style.display = 'none');
+    document.getElementById('sidebar').style.display =
+      'none';
   };
 
   return (
-    <div className='sidebar' id='sidebar'>
+    <div
+      className='sidebar'
+      id='sidebar'
+    >
       <div className='sidebar-container'>
         <p className='sidebar-title'>
-          Navigation
+          SPACE PHONE <br />
+          E-COMMERCE
           <MdClose
             className='close-sidebar'
             onClick={handleCloseSidebar}
           />
         </p>
         <div className='sidebar-list'>
-          <div className='sidebar-link-list'>
-            <Link to='/' className='sidebar-link'>
+          {/* <div className='sidebar-link-list'>
+            <Link
+              to='/login'
+              className='sidebar-link'
+            >
+              <p>Account</p>
+              <RiArrowDropRightLine className='sidebar-item-icon' />
+            </Link>
+            <Link
+              to='/'
+              className='sidebar-link'
+            >
               <p>Home Page</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
             <Link
               to='/all-products'
-              className='sidebar-link'>
+              className='sidebar-link'
+            >
               <p>All Products</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
-            <Link to='/contact-us' className='sidebar-link'>
+            <Link
+              to='/contact-us'
+              className='sidebar-link'
+            >
               <p>Contact Us</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
-            <Link to='/about-us' className='sidebar-link'>
+            <Link
+              to='/about-us'
+              className='sidebar-link'
+            >
               <p>About Us</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
-          </div>
+          </div>*/}
           <div className='sidebar-category'>
-            <p className='sidebar-category-title'>
-              Category
-            </p>
             <Link
-              to='/all-products'
-              className='sidebar-item'>
-              <CiMobile3 className='sidebar-item-icon' />
-              <p>Mobile</p>
+              to='/login'
+              className='sidebar-item'
+            >
+              <RiAccountCircleFill className='sidebar-item-icon' />
+              <p>Account</p>
+              <RiArrowDropRightLine className='sidebar-item-icon' />
+            </Link>
+            <Link
+              to='/'
+              className='sidebar-item'
+            >
+              <HiHome className='sidebar-item-icon' />
+              <p>Home Page</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
             <Link
               to='/all-products'
-              className='sidebar-item'>
-              <CiLaptop className='sidebar-item-icon' />
-              <p>Laptop</p>
+              className='sidebar-item'
+            >
+              <AiFillShop className='sidebar-item-icon' />
+              <p>All Products</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
             <Link
-              to='/all-products'
-              className='sidebar-item'>
-              <IoGameControllerOutline className='sidebar-item-icon' />
-              <p>Gaming</p>
+              to='/contact-us'
+              className='sidebar-item'
+            >
+              <MdContactEmergency className='sidebar-item-icon' />
+              <p>Contact Us</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
             <Link
-              to='/all-products'
-              className='sidebar-item'>
-              <CiPalette className='sidebar-item-icon' />
-              <p>Accessories</p>
+              to='/about-us'
+              className='sidebar-item'
+            >
+              <BsFillInfoCircleFill className='sidebar-item-icon' />
+              <p>About Us</p>
               <RiArrowDropRightLine className='sidebar-item-icon' />
             </Link>
           </div>
